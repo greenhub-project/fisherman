a33672c67f11dc49dc304e3452f754067e0a6522
Hugo Matalonga <hmatalonga@gmail.com>
2020-08-01 21:57:03 +0100

    feat: store data pre-processing metadata in mongo

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,3 +1,59 @@
+import re
+import glob
+import numpy as np
+
 def memory_usage(df, aggregate=True):
-  memory = df.memory_usage(deep=True).sum() if aggregate else df.memory_usage(deep=True)
-  return round(memory / 1024 ** 2, 2)
+    memory = df.memory_usage(index=True, deep=True).sum() if aggregate else df.memory_usage(index=True, deep=True)
+    return round(memory / 1024 ** 2, 2)
+
+def get_csv_files(filepath):
+    # natural order sorting
+    return sorted(glob.glob(filepath), key=lambda x: int(re.findall(r'\d+', x)[0]))
+
+def correct_encoding(dictionary):
+    """Correct the encoding of python dictionaries so they can be encoded to mongodb
+    inputs
+    -------
+    dictionary : dictionary instance to add as document
+    output
+    -------
+    new : new dictionary with (hopefully) corrected encodings"""
+
+    new = {}
+    for key1, val1 in dictionary.items():
+        # Nested dictionaries
+        if isinstance(val1, dict):
+            val1 = correct_encoding(val1)
+
+        if isinstance(val1, np.bool_):
+            val1 = bool(val1)
+
+        if isinstance(val1, np.int64):
+            val1 = int(val1)
+
+        if isinstance(val1, np.float64):
+            val1 = float(val1)
+
+        new[key1] = val1
+
+    return new
+
+def update_metadata(collection, df, page, exclude=['id']):
+    df = df.drop(exclude, axis=1)
+
+    numeric_stats, object_stats = {}, {}
+    
+    try:
+        numeric_stats = df.describe(include=[np.number]).to_dict()
+    except:
+        pass
+
+    try:
+        object_stats = df.describe(exclude=[np.number]).to_dict()
+    except:
+        pass
+
+    entry = {str(page): {**numeric_stats, **object_stats}}
+    entry = correct_encoding(entry)
+
+    return collection.insert_one(entry)
